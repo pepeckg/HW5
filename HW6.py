2ac5312c15f33b4b6599fa8b49b46ca92e1295a9
Van0kgz <205cm@mail.ru>
2023-09-24 19:38:42 +0600

    first

---
+++ HW6.py
@@ -0,0 +1,42 @@
+def selection_sort(any_list):
+    n = len(any_list)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if any_list[j] < any_list[min_index]:
+                min_index = j
+        any_list[i], any_list[min_index] = any_list[min_index], any_list[i]
+
+def binary_search(A, VAL): # A - это список где ищем, VAL - что ищем
+    N = len(A)
+    result_ok = False
+    first = 0
+    last = N - 1
+
+    while first <= last:
+        middle = (first + last) // 2
+        if VAL == A[middle]:
+            first = middle
+            last = first
+            result_ok = True
+            POS = middle
+            break
+        else:
+            if VAL > A[middle]:
+                first = middle + 1
+            else:
+                last = middle - 1
+
+    if result_ok == True:
+        print(f'элемент {VAL} найден под индексом {POS}')
+    else:
+        print(f'элемент {VAL} не найден')
+
+unsorted_list = [38, 35, 44, 34, 43, 37, 33, 41, 31, 45, 46, 47, 42, 50, 30]
+print('неотсортированный список:', unsorted_list)
+
+selection_sort(unsorted_list)
+print('отсортированный список:', unsorted_list)
+
+target_element = 30
+binary_search(unsorted_list, target_element)
